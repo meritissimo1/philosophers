426d88f58a561064b7ce0ea9078afa063b9a583c
Marcelo | marcrodr <marcelomoreira1905@gmail.com>
2023-01-30 12:39:19 -0400

    created struct philo

--- includes/philo.h
+++ includes/philo.h
@@ -6,7 +6,7 @@
 /*   By: marcrodr <marcrodr@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/19 10:28:40 by marcrodr          #+#    #+#             */
-/*   Updated: 2023/01/23 16:37:02 by marcrodr         ###   ########.fr       */
+/*   Updated: 2023/01/30 12:34:43 by marcrodr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,45 @@
 
 # include <stdio.h>
 # include <limits.h>
+# include <stdlib.h>
+# include <stdbool.h>
+
+typedef struct s_param
+{
+	int				philo_nbr;
+	int				time2die;
+	int				time2eat;
+	int				time2sleep;
+	int				meals_nbr;
+	long int		start_time;
+	int				is_dead;
+	pthread_mutex_t	*death;
+	pthread_mutex_t *text;
+	pthread_mutex_t *who;	
+}					t_param;
+
+typedef struct s_philo
+{
+    int				name;
+	long int		last_meal;
+	int				meals;
+	bool			satisfied;
+	int				time_eaten;
+	pthread_mutex_t	*left_fork;
+	pthread_mutex_t	*right_fork;
+	pthread_mutex_t *mutex_meals;
+	pthread_t		philo_thread;
+	t_param			*param;
+}					t_philo;
 
 /*********
 ** UTILS
 **********/
-int	check_args(int agrc, char **argv);
+int				check_args(int agrc, char **argv);
 long long int	ft_itoll(char *str);
-int	ft_atoi(const char *str);
-int	validate_args(int argc);
-int	ft_isdigit(int arg);
+int				ft_atoi(const char *str);
+int				validate_args(int argc);
+int				ft_isdigit(int arg);
 
 
 
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: marcrodr <marcrodr@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/19 10:25:49 by marcrodr          #+#    #+#             */
-/*   Updated: 2023/01/23 16:30:12 by marcrodr         ###   ########.fr       */
+/*   Updated: 2023/01/30 12:34:01 by marcrodr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,11 @@
 
 int	main(int argc, char **argv)
 {
+	t_philo	*philo;
+
 	printf("argc: %d\nargv: %s\n\n", argc, argv[1]);
 	if (validate_args(argc) || check_args(argc, argv))
-		return (1);	
+		return (1);
+	philo = malloc(sizeof(t_philo) *ft_atoi(argv[1]));	
 	return (0);
 }

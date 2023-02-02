4ec102b4e1ab177ba82f0fe82ff32b84161ace77
Marcelo | marcrodr <marcelomoreira1905@gmail.com>
2023-02-02 11:12:48 -0400

    debug in workspace

--- src/actions/dinner.c
+++ src/actions/dinner.c
@@ -6,7 +6,7 @@
 /*   By: marcrodr <marcrodr@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/02 11:00:35 by marcrodr          #+#    #+#             */
-/*   Updated: 2023/02/02 11:02:08 by marcrodr         ###   ########.fr       */
+/*   Updated: 2023/02/02 11:11:29 by marcrodr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ void *dinner(void *arg)
 	t_philo	*philo_n;
 
 	philo_n = (t_philo *)arg;
+	printf ("one philo %i\n", philo_n->param->philo_nbr);
 	pthread_mutex_lock(philo_n->mutex_meals);
 	philo_n->last_meal = philo_n->param->start_time;
 	pthread_mutex_unlock(philo_n->mutex_meals);
